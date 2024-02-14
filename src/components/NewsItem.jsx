5f1651055f2a46ebcbbd7011b011d92b583655e0
Fardaan Mahdi <codingmonk501@gmail.com>
2024-02-14 23:03:52 +0530

    create basic setup using sample data

--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -5,7 +5,7 @@ export class NewsItem extends Component {
         super();
     }
   render() {
-    let {title,description,imageURL}=this.props;
+    let {title,description,imageURL,newsUrl}=this.props;
     return(
         <div className="w-[300px] rounded-md border">
       <img
@@ -14,15 +14,15 @@ export class NewsItem extends Component {
         className="h-[200px] w-full rounded-md object-cover"
       />
       <div className="p-4">
-        <h1 className="text-lg font-semibold">{title}</h1>
+        <h1 className="text-lg font-semibold">{title}...</h1>
         <p className="mt-3 text-sm text-gray-600">
-          {description}
+          {description}...
         </p>
         <button
           type="button"
           className="mt-4 rounded-sm bg-black px-2.5 py-1 text-[10px] font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
         >
-          Read More
+          <a href={newsUrl} target='_blank'>Read More</a>
         </button>
       </div>
     </div>
